15abecc3c699c7e69db1a1c91874af618bce0942
gigamon <gigamon@hotmail.com>
2026-03-11 20:55:42 -0400

    Remove a unique constraint from the ss.stat_period table.
    The ss.stat_period table had a unique constraint on (stat_tracking_id, period_range). This made sense for regular periods (Monthly, Forever) where there would be one period per month for the game type being tracked. However, it doesn't work for League Season periods. There could be multiple seasons for the same game type with the same period. This normally would be very unlikely. However, it is possible.
    
    Also, there's a scenario where it's likely to occur unintentionally:
    1. Create a season
    2. Start the season (which creates the stat period)
    3. Delete the season (the stat period is not deleted)
    4. Create and start another season with the same start date, but the constraint will conflict.

--- Schemas/ss/Tables/stat_period.sql
+++ Schemas/ss/Tables/stat_period.sql
@@ -8,7 +8,6 @@ CREATE TABLE IF NOT EXISTS ss.stat_period
     stat_tracking_id bigint NOT NULL,
     period_range tstzrange NOT NULL,
     CONSTRAINT stat_period_pkey PRIMARY KEY (stat_period_id),
-    CONSTRAINT stat_period_stat_tracking_id_period_range_key UNIQUE (stat_tracking_id, period_range),
     CONSTRAINT stat_period_stat_tracking_id_fkey FOREIGN KEY (stat_tracking_id)
         REFERENCES ss.stat_tracking (stat_tracking_id) MATCH SIMPLE
         ON UPDATE NO ACTION
